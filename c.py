af843b2cf4a943fbed81cba8afc11504e925fd64
shamikadsg <113249062+shamikadsg@users.noreply.github.com>
2022-09-10 20:15:22 +0530

    Update c.py

--- c.py
+++ c.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-for root, dirs, files in os.walk('./hirutv/2'):  # replace the . with your starting directory
+for root, dirs, files in os.walk('./myfile/abc'):  
    for file in files:
       path_file = os.path.join(root,file)
-      shutil.copy2(path_file,'./imgs/test')
+      shutil.copy2(path_file,'./imgs/test1')
